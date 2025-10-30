4f629e54278ab9a38984664493475a9533163696
Ayrik Nabirahni <ayriknabirahni01@gmail.com>
2025-10-30 01:21:50 -0700

    0.1.0 - very basic atm. I want to make a web version of it, but it does work in its current state

--- binacle/binacle.py
+++ binacle/binacle.py
@@ -4,13 +4,12 @@ is apart of the Binacle project, and licensed under
 the GNU AGPL-3.0-or-later. See LICENSE and README for more details.
 """
 
-version = '0.0.2'
 
 import requests
-
+from data.data import *
 
 friendly_pokemon_list = []
-
+enemy_pokemon_list = []
 
 def get_pokemon_info(pokemon_name): # taken from https://medium.com/@mohamed.mywork/learn-apis-with-pok%C3%A9mon-and-python-7003b35b5ba
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
@@ -51,33 +50,104 @@ def extract_moves(moveset):
         moves[index] = moves[index].replace(' ', '-')
     return moves
 
-def initialize_pokemon(name, moveset_string):
+def initialize_pokemon(name, moveset_string, is_enemy):
+    if is_enemy == True:
+        pokemon_dict = { # simplified dictionary
+        'name': None,
+        'types': [],
+        }
 
-    pokemon_dict = {
-    'name': None,
-    'types': [],
-    'moves': None,
-    'move_types': []
-    }
+        pokemon_info = get_pokemon_info(name)
 
-    pokemon_info = get_pokemon_info(name)
+        pokemon_dict['name'] = pokemon_info['name']
+        pokemon_dict['types'].append(pokemon_info['types'])
 
-    pokemon_dict['name'] = pokemon_info['name']
-    pokemon_dict['types'].append(pokemon_info['types'])
+        return pokemon_dict
 
-    moves = extract_moves(moveset_string)
-    pokemon_dict['moves'] = moves
-    for move in moves: # get move types
-        pokemon_dict['move_types'].append(get_move_info(move)['type'])
-    
-    return pokemon_dict
+    elif is_enemy == False:
+        pokemon_dict = {
+        'name': None,
+        'types': [],
+        'moves': None,
+        'move_types': []
+        }
+
+        pokemon_info = get_pokemon_info(name)
+
+        pokemon_dict['name'] = pokemon_info['name']
+        pokemon_dict['types'].append(pokemon_info['types'])
+
+        moves = extract_moves(moveset_string)
+        pokemon_dict['moves'] = moves
+        for move in moves: # get move types
+            pokemon_dict['move_types'].append(get_move_info(move)['type'])
+        
+        return pokemon_dict
+
+
+def calculate_weakness_multiplier(defending_pokemon_types, attacking_move_type):
+    defending_pokemon_dict_list = []
+    multipliers = []
+    weakness = 1
+    for type in defending_pokemon_types:
+        defending_pokemon_dict_list.append(type_defense_dict[type])
+
+    for dict in defending_pokemon_dict_list:
+        multipliers.append(dict[attacking_move_type])
+
+    for multiplier in multipliers:
+        weakness = weakness * multiplier
+
+    return weakness # higher number means move is more effective
+
+
+
 
 
 for x in range (3): # initialize pokemon
     pokemon_name = input("Enter your pokemon's name: ")
     pokemon_moveset = input("Enter Pokemon's moveset, seperated by commas. (ex: Astonish, Dynamic Punch, Sand Tomb): ")
-    friendly_pokemon_list.append(initialize_pokemon(pokemon_name, pokemon_moveset))
-
+    friendly_pokemon_list.append(initialize_pokemon(pokemon_name, pokemon_moveset, False))
 
 
 print(friendly_pokemon_list)
+
+pokemon_number = 'first'
+enemy_pokemon_index = 0
+done = False
+while True:
+    if done == True:
+        end = input('Enter any letter to exit... ')
+        break
+    else:
+
+
+        pokemon_name = input(f"Enter your {pokemon_number} opponent's pokemon's name: ")
+        enemy_pokemon_list.append(initialize_pokemon(pokemon_name, None, True))
+
+
+        highest_weakness = 0
+        optimal_pokemon_index = 0
+        move_index = 0
+        # go through each move for each pokemon. the name of the pokemon and highest overall weakness move will be saved
+        for friendly_pokemon_index in range(len(friendly_pokemon_list)):
+            for move_type_index in range(len(friendly_pokemon_list[friendly_pokemon_index]['move_types'])):
+                current_move_type = friendly_pokemon_list[friendly_pokemon_index]['move_types'][move_type_index]
+                weakness = calculate_weakness_multiplier(enemy_pokemon_list[enemy_pokemon_index]['types'][0], friendly_pokemon_list[friendly_pokemon_index]['move_types'][move_type_index])
+                if weakness > highest_weakness:
+                    highest_weakness = weakness
+                    optimal_pokemon_index = friendly_pokemon_index
+                    move_index = move_type_index
+
+        print(f'Best move: {friendly_pokemon_list[optimal_pokemon_index]['moves'][move_index]} ({friendly_pokemon_list[optimal_pokemon_index]['name']}) with effectiveness of {round(highest_weakness, 3)}x')
+
+
+
+
+        if pokemon_number == 'first':
+            pokemon_number = 'second'
+        elif pokemon_number == 'second':
+            pokemon_number = 'third'
+        elif pokemon_number == 'third':
+            done = True
+        enemy_pokemon_index += 1
---
+++ data/data.py
@@ -0,0 +1,406 @@
+"""
+Copyright (C) 2025 Ayrik Nabirahni. This file
+is apart of the Binacle project, and licensed under
+the GNU AGPL-3.0-or-later. See LICENSE and README for more details.
+"""
+
+version = '0.1.0'
+
+normal_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1,
+    'water': 1,
+    'electric': 1,
+    'grass': 1,
+    'ice': 1,
+    'fighting': 1.6,
+    'poison': 1,
+    'ground': 1,
+    'flying': 1,
+    'psychic': 1,
+    'bug': 1,
+    'rock': 1,
+    'ghost': 0.39,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 1,
+    'fairy': 1
+}
+
+fire_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 0.63,
+    'water': 1.6,
+    'electric': 1,
+    'grass': 0.63,
+    'ice': 0.63,
+    'fighting': 1,
+    'poison': 1,
+    'ground': 1.6,
+    'flying': 1,
+    'psychic': 1,
+    'bug': 0.63,
+    'rock': 1.6,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 0.63,
+    'fairy': 0.63
+}
+
+water_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 0.63,
+    'water': 0.63,
+    'electric': 1.6,
+    'grass': 1.6,
+    'ice': 0.63,
+    'fighting': 1,
+    'poison': 1,
+    'ground': 1,
+    'flying': 1,
+    'psychic': 1,
+    'bug': 1,
+    'rock': 1,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 0.63,
+    'fairy': 1
+}
+
+electric_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1,
+    'water': 1,
+    'electric': 0.63,
+    'grass': 1,
+    'ice': 1,
+    'fighting': 1,
+    'poison': 1,
+    'ground': 1.6,
+    'flying': 1,
+    'psychic': 1,
+    'bug': 1,
+    'rock': 1,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 0.63,
+    'fairy': 1
+}
+
+grass_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1.6,
+    'water': 0.63,
+    'electric': 0.63,
+    'grass': 0.63,
+    'ice': 1.6,
+    'fighting': 1,
+    'poison': 1.6,
+    'ground': 0.63,
+    'flying': 1.6,
+    'psychic': 1,   
+    'bug': 1.6,
+    'rock': 1,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 1,
+    'fairy': 1
+}
+
+ice_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1.6,
+    'water': 1,
+    'electric': 1,
+    'grass': 1,
+    'ice': 0.63,
+    'fighting': 1.6,
+    'poison': 1,
+    'ground': 1,
+    'flying': 1,
+    'psychic': 1,   
+    'bug': 1,
+    'rock': 1.6,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 1.6,
+    'fairy': 1
+}
+
+fighting_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1,
+    'water': 1,
+    'electric': 1,
+    'grass': 1,
+    'ice': 1,
+    'fighting': 1,
+    'poison': 1,
+    'ground': 1,
+    'flying': 1.6,
+    'psychic': 1.6,   
+    'bug': 0.63,
+    'rock': 0.63,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 0.63,
+    'steel': 1,
+    'fairy': 1.6
+}
+
+poison_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1,
+    'water': 1,
+    'electric': 1,
+    'grass': 0.63,
+    'ice': 1,
+    'fighting': 0.63,
+    'poison': 0.63,
+    'ground': 1.6,
+    'flying': 1,
+    'psychic': 1.6,   
+    'bug': 0.63,
+    'rock': 1,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 1,
+    'fairy': 0.63
+}
+
+ground_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1,
+    'water': 1.6,
+    'electric': 0.39,
+    'grass': 1.6,
+    'ice': 1.6,
+    'fighting': 1,
+    'poison': 0.63,
+    'ground': 1,
+    'flying': 1,
+    'psychic': 1,   
+    'bug': 1,
+    'rock': 0.63,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 1,
+    'fairy': 1
+}
+
+flying_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1,
+    'water': 1,
+    'electric': 1.6,
+    'grass': 0.63,
+    'ice': 1.6,
+    'fighting': 0.63,
+    'poison': 1,
+    'ground': 0.39,
+    'flying': 1,
+    'psychic': 1,   
+    'bug': 0.63,
+    'rock': 1.6,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 1,
+    'fairy': 1
+}
+
+psychic_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1,
+    'water': 1,
+    'electric': 1,
+    'grass': 1,
+    'ice': 1,
+    'fighting': 0.63,
+    'poison': 1,
+    'ground': 1,
+    'flying': 1,
+    'psychic': 0.63,   
+    'bug': 1.6,
+    'rock': 1,
+    'ghost': 1.6,
+    'dragon': 1,
+    'dark': 1.6,
+    'steel': 1,
+    'fairy': 1
+}
+
+bug_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1.6,
+    'water': 1,
+    'electric': 1,
+    'grass': 0.63,
+    'ice': 1,
+    'fighting': 0.63,
+    'poison': 1,
+    'ground': 0.63,
+    'flying': 1.6,
+    'psychic': 1,   
+    'bug': 1,
+    'rock': 1.6,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 1,
+    'fairy': 1
+}
+
+rock_type_defense_multipliers = {
+    'normal': 0.63,
+    'fire': 0.63,
+    'water': 1.6,
+    'electric': 1,
+    'grass': 1.6,
+    'ice': 1,
+    'fighting': 1.6,
+    'poison': 0.63,
+    'ground': 1.6,
+    'flying': 0.63,
+    'psychic': 1,   
+    'bug': 1,
+    'rock': 1,
+    'ghost': 1,
+    'dragon': 1,
+    'dark': 1,
+    'steel': 1.6,
+    'fairy': 1
+}
+
+ghost_type_defense_multipliers = {
+    'normal': 0.39,
+    'fire': 1,
+    'water': 1,
+    'electric': 1,
+    'grass': 1,
+    'ice': 1,
+    'fighting': 0.39,
+    'poison': 0.63,
+    'ground': 1,
+    'flying': 1,
+    'psychic': 1,   
+    'bug': 0.63,
+    'rock': 1,
+    'ghost': 1.6,
+    'dragon': 1,
+    'dark': 1.6,
+    'steel': 1,
+    'fairy': 1
+}
+
+dragon_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 0.63,
+    'water': 0.63,
+    'electric': 0.63,
+    'grass': 0.63,
+    'ice': 1.6,
+    'fighting': 1,
+    'poison': 1,
+    'ground': 1,
+    'flying': 1,
+    'psychic': 1,   
+    'bug': 1,
+    'rock': 1,
+    'ghost': 1,
+    'dragon': 1.6,
+    'dark': 1,
+    'steel': 1,
+    'fairy': 1.6
+}
+
+dark_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1,
+    'water': 1,
+    'electric': 1,
+    'grass': 1,
+    'ice': 1,
+    'fighting': 1.6,
+    'poison': 1,
+    'ground': 1,
+    'flying': 1,
+    'psychic': 0.39,   
+    'bug': 1.6,
+    'rock': 1,
+    'ghost': 0.63,
+    'dragon': 1,
+    'dark': 0.63,
+    'steel': 1,
+    'fairy': 1.6
+}
+
+steel_type_defense_multipliers = {
+    'normal': 0.63,
+    'fire': 1.6,
+    'water': 1,
+    'electric': 1,
+    'grass': 0.63,
+    'ice': 0.63,
+    'fighting': 1.6,
+    'poison': 0.39,
+    'ground': 1.6,
+    'flying': 0.63,
+    'psychic': 0.63,   
+    'bug': 0.63,
+    'rock': 0.63,
+    'ghost': 1,
+    'dragon': 0.63,
+    'dark': 1,
+    'steel': 0.63,
+    'fairy': 0.63
+}
+
+fairy_type_defense_multipliers = {
+    'normal': 1,
+    'fire': 1,
+    'water': 1,
+    'electric': 1,
+    'grass': 1,
+    'ice': 1,
+    'fighting': 0.63,
+    'poison': 1.6,
+    'ground': 1,
+    'flying': 1,
+    'psychic': 1,   
+    'bug': 0.63,
+    'rock': 1,
+    'ghost': 1,
+    'dragon': 0.39,
+    'dark': 0.63,
+    'steel': 1.6,
+    'fairy': 1
+}
+
+type_defense_dict = {
+    'normal': normal_type_defense_multipliers,
+    'fire': fire_type_defense_multipliers,
+    'water': water_type_defense_multipliers,
+    'electric': electric_type_defense_multipliers,
+    'grass': grass_type_defense_multipliers,
+    'ice': ice_type_defense_multipliers,
+    'fighting': fighting_type_defense_multipliers,
+    'poison': poison_type_defense_multipliers,
+    'ground': ground_type_defense_multipliers,
+    'flying': flying_type_defense_multipliers,
+    'psychic': psychic_type_defense_multipliers,
+    'bug': bug_type_defense_multipliers,
+    'rock': rock_type_defense_multipliers,
+    'ghost': ghost_type_defense_multipliers,
+    'dragon': dragon_type_defense_multipliers,
+    'dark': dark_type_defense_multipliers,
+    'steel': steel_type_defense_multipliers,
+    'fairy': fairy_type_defense_multipliers
+}
