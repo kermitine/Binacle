a642e547a93222763aaefede989eb801f0621140
Ayrik Nabirahni <ayriknabirahni01@gmail.com>
2025-10-30 17:01:37 -0700

    removed presets

--- data/data.py
+++ data/data.py
@@ -5,12 +5,3 @@ the GNU AGPL-3.0-or-later. See LICENSE and README for more details.
 """
 
 version = '1.0.0'
-
-presets = [
-    {'name': 'dusknoir',
-     'moveset': 'astonish, dynamic punch'},
-    {'name': 'forretress',
-     'moveset': 'bug bite, rock tomb'},
-    {'name': 'dunsparce',
-     'moveset': 'rollout, drill run'}
-       ]
